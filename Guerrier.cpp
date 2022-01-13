e71e5ee0f5b03beaa4f54d7d06de90f4cace2d9c
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-13 11:47:36 +0100

    Update Guerrier.cpp

--- Guerrier.cpp
+++ Guerrier.cpp
@@ -1,10 +1,10 @@
 #include "Guerrier.hpp"
 
 Guerrier::Guerrier():Perso(),nomArme("Epée"){
-  std::cout << "Un véritable guerrier parmi nous? Voilà qui risque d'être sanglant!"<<endl;
+  std::cout << "Un véritable guerrier parmi nous? Voilà qui risque d'être sanglant!"<<std::endl;
 }
 
-Guerrier::coupDepee(Perso adversaire){
+void Guerrier::coupDepee(Perso adversaire){
   adversaire.recoitAttaque(5);
-  std::cout <<"Prends ça mécréant!"<<endl;
+  std::cout <<"Prends ça mécréant!"<<std::endl;
 }
