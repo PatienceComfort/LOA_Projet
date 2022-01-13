88e923c318383017bedec017984912d51806a1d8
PatienceComfort <30979828+PatienceComfort@users.noreply.github.com>
2022-01-13 14:45:03 +0100

    Add files via upload
    Ajout des constructeurs de pièce

--- Piece.cpp
+++ Piece.cpp
@@ -1,1 +1,9 @@
-##include "Piece.hpp"
+#include "Piece.hpp"
+
+
+Piece::Piece():nb_objets(3),presence_adversaire(FALSE){
+  
+}
+Piece::Piece(int nbDePortes):nb_objets(3),presence_adversaire(FALSE), nb_portes(nbDePortes){
+
+}
--- Piece.hpp
+++ Piece.hpp
@@ -1,9 +1,16 @@
 
+#include <vector>
+
 class Piece {
 private:
   /* data */
+  int nb_portes;//une pièce a soit 3 portes, si elle est normale, soit 1 ou 2 si c'est une pièce d'angle
+  int nb_objets; // toujours au nombre de 3
+  vector <string> objets;
+  bool presence_adversaire;//si oui, on va aussi avoir besoin de construire un adversaire
 
 public:
-  Piece (arguments);
+  Piece ();
+  Piece(int nbDePortes);
   virtual ~Piece ();
 };
