45825a7756e6ede57959503573c654383c337cb7
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-13 10:27:34 +0100

    Update Objet.cpp

--- Objet.cpp
+++ Objet.cpp
@@ -1,1 +1,11 @@
-#include "Objet.hpp"
+#include "Objet.h"
+
+Objet::Objet():nbUti(0), niveau(1){}
+
+int Objet::getUti(){
+  return this->nbUti;
+}
+
+int Objet::getNiveau(){
+  return this ->niveau;
+}
