3e4101700f06ace419b398ccdf30766806cf2965
PatienceComfort <30979828+PatienceComfort@users.noreply.github.com>
2022-01-13 19:08:17 +0100

    Add files via upload

--- Perso.cpp
+++ Perso.cpp
@@ -1,5 +1,6 @@
 #include "Perso.h"
 
+
 Perso::Perso():nom(" "), sante(100), hab(1){
   cout << "un personnage a été crée!"<<endl;
 }
@@ -13,5 +14,25 @@ int Perso::getHab(){
 }
 
 void Perso::recoitAttaque(int perte){
-  this->sante -= perte;
+  this->indSante -= perte;
 }
+
+int Perso::getBagCapacity(){
+  return this->sac.size();
+}
+
+int Perso::getRemainSpace(){
+  int remain = 4 - getBagCapacity();
+  return remain;
+}
+
+void Perso::addToBag(Objet obj){
+  this -> sac.insert(obj);
+}
+
+//celui là va necessiter la position de l'objet à enlever
+/*
+void Perso::removeFromBag(Objet obj){
+  this ->
+  //sac.erase(object position)
+}*/
--- Perso.h
+++ Perso.h
@@ -1,18 +1,27 @@
 #ifndef PERSO_H
 #define PERSO_H
 
+#include <vector>
+#include "Objet.hpp"
 class Perso{
 protected:
   std::string nom;
   int sante;
   int hab;
   std::string nomArme;
+  //ajouter le sac
+  std::vector <Objet> sac;
 
 public:
   Perso();
   int getSante();
   int getHab();
   void recoitAttaque(int perte);
+  int getBagCapacity();
+  int getRemainSpace();
+  void addToBag(Objet obj);
+  //void removeFromBag(Objet obj);
+
 };
 
 //notons que l'augmentation de l'indice d'habilité vient avec une augmentation de la capacité du sac
