1167739abcd0888545fc68f3268fdcbc49f8f508
PatienceComfort <30979828+PatienceComfort@users.noreply.github.com>
2022-01-13 14:49:40 +0100

    Perso.cpp

--- Perso.cpp
+++ Perso.cpp
@@ -1,1 +1,17 @@
-#include "Perso.hpp"
+#include "Perso.h"
+
+Perso::Perso():nom(" "), sante(100), hab(1){
+  cout << "un personnage a été crée!"<<endl;
+}
+
+int Perso::getSante(){
+  return this->sante;
+}
+
+int Perso::getHab(){
+  return this ->hab;
+}
+
+void Perso::recoitAttaque(int perte){
+  this->indSante -= perte;
+}
