0316dd191175f959b68d11c0b5c934350c359179
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-13 11:48:21 +0100

    Update Amazone.cpp

--- Amazone.cpp
+++ Amazone.cpp
@@ -1,10 +1,10 @@
 #include "Amazone.hpp"
 
 Amazone::Amazone():Perso(),nomArme("Lance"){
-  std::cout << "Une amazone! Et moi qui comptais faire une banquet après. Je n'auarais plus d'invités!!"<<endl;
+  std::cout << "Une amazone! Et moi qui comptais faire une banquet après. Je n'auarais plus d'invités!!"<<std::endl;
 }
 
 void Amazone::coupDeLance(Perso adversaire){
   adversaire.recoitAttaque(15);
-  std::cout<<"Un coup de lance bien placé suffit!!"<<endl;
+  std::cout<<"Un coup de lance bien placé suffit!!"<<std::endl;
 }
