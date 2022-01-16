46918bbb22b773f59de3207566f8395eacbcf4e6
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-16 11:36:46 +0100

    Update Sorciere.cpp

--- Sorciere.cpp
+++ Sorciere.cpp
@@ -4,7 +4,7 @@ Sorciere::Sorciere():Perso(), nomArme("Baguette Magique"){
   std::cout << "Ahhh! Une sorcière!! Redoutable"<<std::endl;
 }
 
-void Sorciere::attaqueMagique(Perso adversaire){
-  adversaire.recoitAttaque(5);
+void Sorciere::attaqueMagique(Perso * adversaire){
+  adversaire->recoitAttaque(5);
   std::cout <<"Attaque magique!"<<std::endl;
 }
