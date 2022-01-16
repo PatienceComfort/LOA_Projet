9763ff094bd4008a0b805db9559b6457eb4b7829
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-16 20:06:25 +0100

    Update Guerrier.cpp

--- Guerrier.cpp
+++ Guerrier.cpp
@@ -8,3 +8,7 @@ void Guerrier::coupDepee(Perso * adversaire){
   adversaire->recoitAttaque(5);
   std::cout <<"Prends ça mécréant!"<<std::endl;
 }
+
+void Guerrier::presentation_adv(){
+  std::cout << "Ah ah je vais t'écraser !" << std::endl;
+}
