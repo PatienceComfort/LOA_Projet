8fa2ae28118a1b0cc2939dc8f97a2d4964db6798
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-16 11:55:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
+#include <random>
 using namespace std;
 #include "Perso.h"
-#include "Sorciere.hpp"
-#include "Moine.hpp"
-#include "Guerrier.hpp"
-#include "Amazone.hpp"
-#include "Piece.hpp"
+#include "Piece.h"
+#include "Sorciere.h"
+#include "Moine.h"
+#include "Guerrier.h"
+#include "Amazone.h"
 
-
-
-int main() {
-  //Recupérer le nom du personnage
+int main(){
+    //Recupérer le nom du personnage
   string name;
   cout << "Votre nom? " << name << endl;
   cin >> name;
@@ -22,78 +21,112 @@ int main() {
   cout << "Des M pour Moines:"<<endl;
   cout << "Des A pour Amazones:"<<endl;
 
-  //Recupérer le rôle du personnage
-  //Au lieu d'un switch, je vais utiliser if else parce que le switch pose problème avec la construction de personnage
-  //Quoique, c'est peut-être juste mon souci de compilateur
+    //Récupérer le role du personnage 
   char role;
   cin >> role;
-
-  if (role == 'S'){
+    if (role == 'S'){
     cout<<"Ah! une nouvelle sorcière"<<endl;
     cout<<"Eh bien entrez donc."<<endl;
     //Faire constructeur Sorcière
-    Sorciere nom_personage;
+    Perso *nom_perso = new Sorciere;
+    //Message de bienvenue
+    cout << "Entrez donc dans le chateau et essayez de sortir de l'autre coté si vous pouvez"<<endl;
+    cout << "A l'intérieur vous attends un labyrinth interminable, des pièges et des adversaires"<<endl;
+    cout << "Le chateau n'est pas méchant et vous offrira de quoi reussir votre quete. Mais à vous d'en faire bon usage"<<endl;
+    cout << "Combattez vos adversaires, et utilisez vos potion. Mais surtout, choissisez la bonne porte"<<endl;
+    //Creation de la première pièce
+        //nb de portes
+    const std::vector<int> option_portes{1, 2, 3};
+    int door = rand() % option_portes.size();
+    int num_portes = option_portes[door];
+        //presence adversaire
+    bool adv = rand()%2;
+    Piece *piece_active = new Piece(num_portes, adv);
+      //Choix des objets à mettre dans la pièce
+    const std::vector<int> option_objets{1, 2, 3, 4};
+    //objet1
+    int object1 = rand() % option_objets.size();
+    //objet2
+    int object2 = rand() % option_objets.size();
+    //objet3
+    int object3 = rand() % option_objets.size();
+    //Presentation des objets dans la pièce
+    const std::vector<string> objets_noms{"Arme", "Bouclier", "Potion", "Potion"};
+    cout << "Dans cette pièce, il y a trois objets: "<<endl;
+    cout << "- 1 " << objets_noms[object1]<<endl;
+    cout << "- 2 " << objets_noms[object2]<<endl;
+    cout << "- 3 " << objets_noms[object3]<<endl;
+    
+    //boucle if pour presentation d'un adversaire
+    if(adv == true){
+        //nombre aléatoire pour déterminer quel type d'adversaire
+        cout << "Oh tiens nous avons un adversaire"<<endl;
+        //l'adversaire se présente et lance le premier coup
+        const std::vector<int> option_adv{1, 2, 3};
+        int indAdv = rand() % option_adv.size();
+        //faire ça 5 fois
+        if(indAdv == 1){
+            //l'adversaire est un moine
+            Moine *advers = new Moine;
+            //Le moine dit sa petite phrase de moquerie
+            //advers->presentation_adv();
+            advers->coupDePoignard(nom_perso);
+            
+        }else if (indAdv == 2){
+            //l'adversaire est guerrier
+            Guerrier *advers = new Guerrier; 
+            //le guerrier dit un truc méchant
+            //advers->presentation_adv();
+            advers->coupDepee(nom_perso);
+           
+        }else{
+            //l'adversaire est une amazone
+            Amazone *advers = new Amazone;
+            //l'amazone dit qu'elle va l'éventrer
+            //advers->presentation_adv();
+            advers->coupDeLance(nom_perso);
+        }
+        
+    }
+    cout << "Dans votre sac, vous avez de la place pour "<<nom_perso->getRemainSpace()<<" objets"<< endl;
+    cout <<"Quel objet voulez-vous prendre?"<<endl; //il n'aura le droit de prendre qu'un objet par pièce. Meme si son sac est vide au départ
+    int choix_objet;
+    cin >> choix_objet;
+    if(choix_objet == 1){
+        //créer l'objet en question
+        //faire la fonction addToBag
+    }else if(choix_objet == 2){
+        //créer l'objet en question
+        //faire la fonction addToBag
+    }else if (choix_objet == 3){
+        //créer l'objet en question
+        //faire la fonction addToBag
+    }else{
+        cout << "Cet objet n'est pas dans cette pièce. Désolé"<<endl;
+    }
+
   }else if(role=='G'){
     cout<<"Ah! un nouveau guerrier"<<endl;
     cout<<"Eh bien entrez donc."<<endl;
     //Faire constructeur Guerrier
-    Guerrier nom_personage;
+    
   }else if(role == 'M'){
     cout<<"Ah! un nouveau Moine"<<endl;
     cout<<"Eh bien entrez donc."<<endl;
     //Faire constructeur Moine
-    Moine nom_personage;
+    
   }else if(role == 'A'){
     cout<<"Ah! une nouvelle Amazone"<<endl;
     cout<<"Eh bien entrez donc."<<endl;
     //Faire constructeur Amazone
-    Amazone nom_personage;
+    
   }else{
     cout <<"Nous ne connaissons pas ce rôle. Pas dans ce chateau. Allez vous en et revenez quand vous savez ce que vous voulez"<<endl;
     return 0;
   }
 
-  //Message de bienvenue
- cout << "Entrez donc dans le chateau et essayez de sortir de l'autre coté si vous pouvez"<<endl;
- cout << "A l'intérieur vous attends un labyrinth interminable, des pièges et des adversaires"<<endl;
- cout << "Le chateau n'est pas méchant et vous offrira de quoi reussir votre quete. Mais à vous d'en faire bon usage"<<endl;
- cout << "Combattez vos adversaires, et utilisez vos potion. Mais surtout, choissisez la bonne porte"<<endl;
+    //Création du personnage approprié
+    /*Mon problème vient de la construction d'une personnage. Donc je vais repartir là dessus dès que je rentre*/
 
- //Creation de la pièce
- //probleme potentiel: out of scope du vecteur
-  const std::vector<int> option_portes{1, 2, 3};
-  int door = rand() % option_portes.size();
-  int num_portes = option_portes[door];
-  Piece piece_active(num_portes); //donc on crée une pièce avec 1, 2 ou trois portes.
-
-  //Choix des objets à mettre dans la pièce
-  const std::vector<int> option_objets{1, 2, 3, 4};
-  //objet1
-  int object1 = rand() % option_objets.size();
-  //objet2
-  int object2 = rand() % option_objets.size();
-  //objet3
-  int object3 = rand() % option_objets.size();
-  const std::vector<string> objets_noms{"Arme", "Bouclier", "Potion", "Potion"};
-  cout << "Dans cette pièce, il y a trois objets: "<<endl;
-  cout << "- 1" << objets_noms[object1]<<endl;
-  cout << "- 1" << objets_noms[object2]<<endl;
-  cout << "- 1" << objets_noms[object3]<<endl;
-  //a ajouter dans le fichier PERSO
-  /*
-  - get remaining size of bag (int)
-  - get weapon type (string)
-  */
-  cout << "Dans votre sac, vous avez de la place pour" << endl;
-  //Puis message avec commandes
-
-/*
-
-   3 objets aléatoires sont crées
-
-   Le nom de ces objets sont ajoutés dans le vecteur d'objets de la Piece
-   un nombre aléatoire est choisit, si personnage créé on change le bool de presence_adversaire à construire
-   */
-
-  return 0;
+    return 0;
 }
