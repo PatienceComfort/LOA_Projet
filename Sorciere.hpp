8822dfafd0c63a87e099440d84cd9eb08d00b0ca
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-16 11:36:29 +0100

    Update Sorciere.hpp

--- Sorciere.hpp
+++ Sorciere.hpp
@@ -5,7 +5,7 @@ private:
 
 public:
   Sorciere ();
-  void attaqueMagique(Perso adversaire);//la soricère lance une attaque magique qui retire 5 points de vie
+  void attaqueMagique(Perso * adversaire);//la soricère lance une attaque magique qui retire 5 points de vie
   void protego();//utilise bouclier dans le sac 
   virtual ~Sorciere ();
 };
