8c72585b3c1fe47c33db19a573e05a308c98ae17
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-19 11:14:53 +0100

    Delete Moine.hpp

--- Moine.hpp
+++
@@ -1,9 +0,0 @@
-class Moine:public Perso {
-private:
-  std::string nomArme;
-
-public:
-  Moine ();
-  void coupDePoignard(Perso * adversaire);//un coup de Poignard retire 10 points de vie à l'adversaire
-  virtual ~Moine ();
-};
