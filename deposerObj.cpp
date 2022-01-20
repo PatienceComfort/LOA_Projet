ad2414480439a3276b8ef0631fdc8f82b0114799
Shaina1310 <96741959+Shaina1310@users.noreply.github.com>
2022-01-20 22:27:13 +0100

    Add files via upload

--- deposerObj.cpp
+++ deposerObj.cpp
@@ -10,10 +10,10 @@ void depotObjet(Sac * sac){
     }
     else{
         int choixDepot;
-        cout<<"Voulez vous deposer le dernier objet de votre sac (Oui = 1, Non = 0"<<endl;
+        cout<<"Voulez vous deposer le dernier objet de votre sac (Oui: 1, Non: 0)"<<endl;
         cin>>choixDepot;
         if(choixDepot==1){
-            sac->removeFromBag(sac->getBagCapacity());
+            sac->removeFromBag(sac->getBagCapacity()-1);
         }
     }
 }
